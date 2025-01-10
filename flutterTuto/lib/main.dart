ff5078b5717c622c824f15170ecf5045703a805f
p-random <77910497+p-random@users.noreply.github.com>
2025-01-10 11:33:48 +0900

    main.dart before using StatefulWidget

--- flutterTuto/lib/main.dart
+++ flutterTuto/lib/main.dart
@@ -28,23 +28,23 @@ class MyApp extends StatelessWidget {
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
   var likeList = <WordPair>[];
-  
+
   void update() {
     current = WordPair.random();
     notifyListeners();
   }
-  
-  void updateLiked(){
-    if(likeList.contains(current)){
+
+  void updateLiked() {
+    if (likeList.contains(current)) {
       likeList.remove(current);
-    } else{
+    } else {
       likeList.add(current);
     }
     notifyListeners();
   }
 }
 
-class Bigcard extends StatelessWidget{
+class Bigcard extends StatelessWidget {
   const Bigcard({
     super.key,
     required this.pair,
@@ -53,64 +53,98 @@ class Bigcard extends StatelessWidget{
   final WordPair pair;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final style = theme.textTheme.bodyMedium!.copyWith(
-      color : theme.colorScheme.onPrimary,
+      color: theme.colorScheme.onPrimary,
     );
     return Card(
       color: theme.colorScheme.primary,
-      child :
-      Padding(
-          padding : const EdgeInsets.all(20),
-          child : Text(pair.asLowerCase, style : style,)
-      ),
+      child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text(
+            pair.asLowerCase,
+            style: style,
+          )),
     );
+  }
+}
+
+class Generator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var mixedText = appState.current;
+
+    IconData icon;
+    if (appState.likeList.contains(mixedText)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Bigcard(pair: mixedText),
+          SizedBox(height: 5),
+          Row(mainAxisSize: MainAxisSize.min, children: [
+            ElevatedButton.icon(
+              onPressed: () {
+                appState.updateLiked();
+              },
+              icon: Icon(icon),
+              label: Text('Like'),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  appState.update();
+                },
+                child: Text('next')),
+          ]),
+        ],
+      ),
+    ));
   }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var mixedText = appState.current;
-    
-    IconData icon;
-    if(appState.likeList.contains(mixedText)){
-      icon = Icons.favorite;
-    }else{
-      icon = Icons.favorite_border;
-    }
-    
     return Scaffold(
-        body: Center(
-          child : Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Bigcard(
-                  pair : mixedText
-              ),
-              SizedBox(height : 5),
-              Row(
-                mainAxisSize : MainAxisSize.min,
-                children : [
-                  ElevatedButton.icon(
-                    onPressed : (){
-                      appState.updateLiked();
-                    },
-                    icon : Icon(icon),
-                    label : Text('Like'),
-                  ),
-                  ElevatedButton(
-                  onPressed: () {
-                    appState.update();
-                  },
-                  child: Text('next')),
-                ]
-              ),
-            ],
+        body: Row(
+      children: [
+        SafeArea(
+          child: Container(
+            child: NavigationRail(
+              backgroundColor: Colors.white,
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Like'),
+                ),
+              ],
+              selectedIndex: 0,
+              onDestinationSelected: (value) {
+                print('selected: $value');
+              },
+            ),
           ),
-        )
-    );
+        ),
+        Expanded(
+          child: Container(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            child: Generator(),
+          ),
+        ),
+      ],
+    ));
   }
 }
