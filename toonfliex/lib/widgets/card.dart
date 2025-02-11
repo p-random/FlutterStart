861f97e910a4ed04bcbe6617b5f8bac530247dff
p-random <jerry123450@naver.com>
2025-02-11 16:50:21 +0900

    config

--- toonfliex/lib/widgets/card.dart
+++ toonfliex/lib/widgets/card.dart
@@ -4,6 +4,7 @@ class MyCard extends StatelessWidget {
   final bool isInverted;
   final String where, smallwhere, much;
   final IconData icon;
+  final double offset;
 
   const MyCard(
       {super.key,
@@ -11,59 +12,63 @@ class MyCard extends StatelessWidget {
       required this.where,
       required this.much,
       required this.smallwhere,
-      required this.icon});
+      required this.icon,
+      required this.offset});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-          color: isInverted ? Colors.white : Color(0xFF1F2123),
-          borderRadius: BorderRadius.circular(20)),
-      child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(where,
-                    style: TextStyle(
-                      color: isInverted ? Colors.black : Colors.white,
-                      fontSize: 20,
-                    )),
-                SizedBox(height: 7),
-                Row(
-                  children: [
-                    Text(much,
-                        style: TextStyle(
-                            color: isInverted
-                                ? Colors.black
-                                : Colors.white.withOpacity(0.5))),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text(smallwhere,
-                        style: TextStyle(
-                            color: isInverted
-                                ? Colors.black
-                                : Colors.white.withOpacity(0.5)))
-                  ],
-                ),
-              ],
-            ),
-            Transform.translate(
-              offset: Offset(-20, 17),
-              child: Transform.scale(
-                scale: 5,
-                child: Icon(
-                  icon,
-                  color: isInverted ? Colors.black : Colors.white,
-                ),
+    return Transform.translate(
+      offset: Offset(0, offset * -20),
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+            color: isInverted ? Colors.white : Color(0xFF1F2123),
+            borderRadius: BorderRadius.circular(20)),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(where,
+                      style: TextStyle(
+                        color: isInverted ? Colors.black : Colors.white,
+                        fontSize: 20,
+                      )),
+                  SizedBox(height: 7),
+                  Row(
+                    children: [
+                      Text(much,
+                          style: TextStyle(
+                              color: isInverted
+                                  ? Colors.black
+                                  : Colors.white.withOpacity(0.5))),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(smallwhere,
+                          style: TextStyle(
+                              color: isInverted
+                                  ? Colors.black
+                                  : Colors.white.withOpacity(0.5)))
+                    ],
+                  ),
+                ],
               ),
-            )
-          ],
+              Transform.translate(
+                offset: Offset(-20, 17),
+                child: Transform.scale(
+                  scale: 5,
+                  child: Icon(
+                    icon,
+                    color: isInverted ? Colors.black : Colors.white,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
