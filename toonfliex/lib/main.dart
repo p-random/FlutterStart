9446e827fdbbae2e2c1474571e281342987a6f16
p-random <jerry123450@naver.com>
2025-02-11 16:25:42 +0900

    card plus

--- toonfliex/lib/main.dart
+++ toonfliex/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'widgets/button.dart';
+import 'widgets/card.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,75 +14,129 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: 1000,
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        "Hey, Selena",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                      Text(
-                        "Welcome back",
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.5),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 100,
-              ),
-              Text(
-                "Total Balance",
-                style: TextStyle(
-                    color: Colors.white.withOpacity(0.5), fontSize: 25),
-              ),
-              Text(
-                "\$3 502 450",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 50,
-                  fontWeight: FontWeight.w700,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 1000,
+                  height: 20,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Button(
-                    text: "Transfer",
-                    backcolor: Colors.amber,
-                    textcolor: Colors.black,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          "Hey, Selena",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          "Welcome back",
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.5),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 60,
+                ),
+                Text(
+                  "Total Balance",
+                  style: TextStyle(
+                      color: Colors.white.withOpacity(0.5), fontSize: 25),
+                ),
+                Text(
+                  "\$3 502 450",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w700,
                   ),
-                  SizedBox(width: 30),
-                  Button(
-                    text: "Request",
-                    backcolor: Color(0xFF1F2123),
-                    textcolor: Colors.white,
-                  ),
-                ],
-              ),
-            ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Button(
+                      text: "Transfer",
+                      backcolor: Colors.amber,
+                      textcolor: Colors.black,
+                    ),
+                    SizedBox(width: 30),
+                    Button(
+                      text: "Request",
+                      backcolor: Color(0xFF1F2123),
+                      textcolor: Colors.white,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Wallets",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 30,
+                      ),
+                    ),
+                    Text(
+                      "View All",
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.5),
+                        fontWeight: FontWeight.w200,
+                        fontSize: 10,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                MyCard(
+                    isInverted: false,
+                    where: "Euro",
+                    much: "6 428",
+                    smallwhere: "EUR",
+                    icon: Icons.euro),
+                Transform.translate(
+                  offset: Offset(0, -15),
+                  child: MyCard(
+                      isInverted: true,
+                      where: "Bitcoin",
+                      much: "9 785",
+                      smallwhere: "BTC",
+                      icon: Icons.currency_bitcoin),
+                ),
+                Transform.translate(
+                  offset: Offset(0, -30),
+                  child: MyCard(
+                      isInverted: false,
+                      where: "Dollar",
+                      much: "428",
+                      smallwhere: "USD",
+                      icon: Icons.currency_exchange),
+                ),
+              ],
+            ),
           ),
         ),
       ),
---
+++ toonfliex/lib/widgets/card.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class MyCard extends StatelessWidget {
+  final bool isInverted;
+  final String where, smallwhere, much;
+  final IconData icon;
+
+  const MyCard(
+      {super.key,
+      required this.isInverted,
+      required this.where,
+      required this.much,
+      required this.smallwhere,
+      required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+          color: isInverted ? Colors.white : Color(0xFF1F2123),
+          borderRadius: BorderRadius.circular(20)),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(where,
+                    style: TextStyle(
+                      color: isInverted ? Colors.black : Colors.white,
+                      fontSize: 20,
+                    )),
+                SizedBox(height: 7),
+                Row(
+                  children: [
+                    Text(much,
+                        style: TextStyle(
+                            color: isInverted
+                                ? Colors.black
+                                : Colors.white.withOpacity(0.5))),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(smallwhere,
+                        style: TextStyle(
+                            color: isInverted
+                                ? Colors.black
+                                : Colors.white.withOpacity(0.5)))
+                  ],
+                ),
+              ],
+            ),
+            Transform.translate(
+              offset: Offset(-20, 17),
+              child: Transform.scale(
+                scale: 5,
+                child: Icon(
+                  icon,
+                  color: isInverted ? Colors.black : Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
